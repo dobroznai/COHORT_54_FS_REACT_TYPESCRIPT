56209d8030f2fb76dc233c3620b73cdad962f507
Ivan Dobroznai <i.dobroznai@gmail.com>
2025-08-17 22:34:49 +0200

    lesson_06

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import Lesson_06 from "./lessons/Laesson_06/Laesson_06";
+import Lesson_06 from "./lessons/Lesson_06/Lesson_06";
+
+import Homework_06 from "./homeworks/Homework_06/Homework_06";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <Lesson_06 />
+      {/* // lessons */}
+      {/* <Lesson_06 /> */}
+      {/* // homeworks */}
+      {/* <Homework_06 /> */}
+      <Homework_06 />
     </div>
   );
 }
