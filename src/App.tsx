97fcacf91a6f4056cf693f943ad4675145bfd234
Ivan Dobroznai <i.dobroznai@gmail.com>
2025-08-21 22:36:57 +0200

    added '@types/node':'^24.3.0'

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 // import Lesson_06 from "./lessons/Lesson_06/Lesson_06";
 
 // import Homework_06 from "./homeworks/Homework_06/Homework_06";
-import Homework_06 from "./lessons/Lesson_07/Homework_06/Homework_06";
+// import Homework_06 from "./lessons/Lesson_07/Homework_06/Homework_06";
+
+//Homework
+import Homework_06 from "lessons/Lesson_07/Homework_06/Homework_06";
 
 import "./App.css";
 
