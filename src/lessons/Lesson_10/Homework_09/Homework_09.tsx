980b5e39ca5bfb3c168365af1541e641ad5e9fe8
Ivan Dobroznai <i.dobroznai@gmail.com>
2025-09-10 19:24:47 +0200

    refactor: added axios in homework_09

--- src/lessons/Lesson_10/Homework_09/Homework_09.tsx
+++ src/lessons/Lesson_10/Homework_09/Homework_09.tsx
@@ -1,15 +1,10 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 import Button from "components/Button/Button";
 
 import { RANDOM_JOKE_URL } from "./data";
-import {
-  PageWrapper,
-  Text,
-  JokeContainer,
-  Card,
-  ErrorText,
-} from "./Homework_09/styles";
+import { PageWrapper, Text, JokeContainer, Card, ErrorText } from "./styles";
 
 function Homework_09() {
   const [joke, setJoke] = useState<undefined | string>(undefined);
@@ -20,22 +15,35 @@ function Homework_09() {
     setJoke(undefined);
     setError(undefined);
     setIsDisabled(true);
-
+    ////////////////////////////////////////////////////////
+    // Пример запроса через библиотеку axios
+    try {
+      const response = await axios.get(RANDOM_JOKE_URL);
+      const data = response.data;
+      // тут пишем логику, когда нам пришла шутка(статус промиса fullfiled)
+      setJoke(`${data.setup} ${data.punchline}`);
+    } catch (error: any) {
+      // тут пишем логику, когда нам пришла ошибка(статус промиса rejected)
+      console.log(error);
+      setError(error.message);
+    } finally {
+      setIsDisabled(false);
+    }
     ///////////////////////////////////////////////////////////
     // Пример запроса через метод fetch()
-    const response = await fetch(RANDOM_JOKE_URL);
-    // В данный момент в result будет леажть либо ошибка(promise венет статус rejected), либо успешно пришедшие данные(promise вернет статут fullfiled)
-    const result = await response.json();
+    // const response = await fetch(RANDOM_JOKE_URL);
+    // // В данный момент в result будет леажть либо ошибка(promise венет статус rejected), либо успешно пришедшие данные(promise вернет статут fullfiled)
+    // const result = await response.json();
 
-    if (response.ok) {
-      // тут пишем логику, когда нам пришла шутка(статус промиса fullfiled)
-      setIsDisabled(false);
-      setJoke(`${result.setup} ${result.punchline}`);
-    } else {
-      // тут пишем логику, когда нам пришла ошибка(статус промиса rejected)
-      setIsDisabled(false);
-      setError("Some Network Error");
-    }
+    // if (response.ok) {
+    //   // тут пишем логику, когда нам пришла шутка(статус промиса fullfiled)
+    //   setIsDisabled(false);
+    //   setJoke(`${result.setup} ${result.punchline}`);
+    // } else {
+    //   // тут пишем логику, когда нам пришла ошибка(статус промиса rejected)
+    //   setIsDisabled(false);
+    //   setError("Some Network Error");
+    // }
   };
 
   useEffect(() => {
