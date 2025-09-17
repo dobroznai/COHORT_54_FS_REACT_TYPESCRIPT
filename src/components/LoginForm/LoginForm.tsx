a0abb1467a2e818fec4b9049fba1b8cacc7003f4
Ivan Dobroznai <i.dobroznai@gmail.com>
2025-09-17 20:34:38 +0200

    Yup

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,6 +1,7 @@
-import { type FormEvent, type ChangeEvent, useState } from "react";
+// import { type FormEvent, type ChangeEvent, useState } from "react";
 // ШАГ 1: импортируем формик из библиотеки формик
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 import Input from "components/Input/Input";
 import Button from "components/Button/Button";
@@ -36,22 +37,34 @@ function LoginForm() {
   //   console.log("Password :", password);
   // };
   //////////////////////////////////////////////////////////////////////
-
+  // Валидация с помощью yup
+  const vakidationSchema = Yup.object().shape({
+    [LOGIN_FORM_VALUES.EMAIL]: Yup.string()
+      .required("Email field is required")
+      .email("This field should be in email forward"),
+    [LOGIN_FORM_VALUES.PASSWORD]: Yup.string()
+      .required("Password field is required")
+      .min(5, "Password field should contain minimum 5 characters")
+      .max(20, "Password field should contain maximum 20 characters"),
+  });
   /////////////////////////////////////////////////////////////////////
-  //  useFormik - возвращает обьект в котором прописаны данные о фориме и вспомогательные функции для работы с формой
+  //  useFormik - возвращает обьект в котором прописаны данные о форме и вспомогательные функции для работы с формой
   // useFormik принимает в качестве аргумента обьект - он является обьязательныи
   // обьект который мы передаем useFormik нужен для настройки обьекта, который возвращает хук useFormik
-  // обьект который мы передаем useFormik имеет 3 обьязательных ключа:
+  // обьект который мы передаем useFormik имеет 2 обьязательных ключа:
   // 1. InitialValues - принимает в качестве значения обьект:
   // - в качестве ключа у нас выступает значение атрибута name в элементах формы
   // - в качестве значения ключа иы прописываем значение по умолчанию которое должно быть в элементе формы
-  // 2. onSubmit - это функия которая вызываетмя на событие submit (при клике на кнопку с type="submit")
+  // 2. onSubmit - это функция которая вызывается на событие submit (при клике на кнопку с type="submit")
 
   const formik = useFormik({
     initialValues: {
       [LOGIN_FORM_VALUES.EMAIL]: "",
       [LOGIN_FORM_VALUES.PASSWORD]: "",
     },
+    validationSchema: vakidationSchema,
+    validateOnMount: false,
+    validateOnChange: false,
     onSubmit: (values, helpers) => {
       // values - это обьект, ключи которого являются значение атрибута name у элемента формы, а значение ключа - это то что ввел пользователь
       // в наших элементах формы.
@@ -59,10 +72,12 @@ function LoginForm() {
       // helpers - вспомогательные функции, которые мы можем использовать во время событий submit
       // например helpers.resetForm(); - очищает форму от значений (т.е. делает форму по умолчанию)
       console.log(helpers);
-      helpers.resetForm();
+      // helpers.resetForm();
       console.log("Submit from formik works!!!");
     },
   });
+
+  console.log(formik);
 
   // formik.handleSubmit мы прописываем для того чтобы когда мы нажали на кнопку с type="submit",
   // у нас вызвалась функция которую мы прописали в onSubmit
@@ -82,6 +97,7 @@ function LoginForm() {
           // formik.handleChange - помогает контолировать ввод пользователя данных с клавиатуры
           // когда пользователь что то вводит, то formik.handleChange записываат данные в formik.values.email
           onChange={formik.handleChange}
+          error={formik.errors[LOGIN_FORM_VALUES.EMAIL]}
         />
         <Input
           id="password_id"
@@ -91,6 +107,7 @@ function LoginForm() {
           label="Password"
           value={formik.values[LOGIN_FORM_VALUES.PASSWORD]}
           onChange={formik.handleChange}
+          error={formik.errors[LOGIN_FORM_VALUES.PASSWORD]}
         />
       </InputContainer>
       {/* <input value={inputValue} onChange={changeInputValue} /> */}
